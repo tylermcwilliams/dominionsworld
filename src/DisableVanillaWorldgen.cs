7c25440886aa862c0728c0d5e235b67c99cf33b3
Tyler McWilliams <tyler.ellis.mcwilliams@gmail.com>
2020-03-19 13:06:59 +0100

    blank start

---
+++ src/DisableVanillaWorldgen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.ServerMods;
+
+// class author: Erik3003
+
+/** Disables:
+ * GenTerra
+ * GenBlockLayers
+ * GenMaps
+ */
+
+namespace customworld
+{
+    public class DisableVanillaWorldGen : ModSystem
+    {
+        private ICoreServerAPI api;
+        private ICoreAPI coreApi;
+
+        public override double ExecuteOrder()
+        {
+            return 0;
+        }
+
+        public override bool ShouldLoad(EnumAppSide forSide)
+        {
+            return forSide == EnumAppSide.Server;
+        }
+
+        public override void Start(ICoreAPI api)
+        {
+            this.coreApi = api;
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            this.api = api;
+            api.Event.ServerRunPhase(EnumServerRunPhase.LoadGamePre, WipeVanillaWorldgenHandlers);
+        }
+
+        void WipeVanillaWorldgenHandlers()
+        {
+            //worldtype
+            IWorldGenHandler handlergroup = this.api.Event.GetRegisteredWorldGenHandlers("standard");
+            foreach (var handlers in handlergroup.OnChunkColumnGen)
+            {
+                for (int i = 0; handlers != null && i < handlers.Count; i++)
+                {
+                    if (handlers[i].Target.GetType().Namespace == "Vintagestory.ServerMods" && (handlers[i].Target.GetType().Name == "GenTerra"))
+                    {
+                        handlers.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+
+            {
+                var handlers = handlergroup.OnMapRegionGen;
+                for (int i = 0; handlers != null && i < handlers.Count; i++)
+                {
+                    if (handlers[i].Target.GetType().Namespace == "Vintagestory.ServerMods" && handlers[i].Target.GetType().Name == "GenMaps" || handlers[i].Target.GetType().Name == "GenBlockLayers")
+                    {
+                        handlers.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/WorldMap.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace customworld
+{
+    public static class WorldMap
+    {
+
+        public static Bitmap map = null;
+        public static Bitmap climateMap = null;
+        // Colors
+        public static Color ocean = Color.FromArgb(0, 0, 0);
+        public static Color abyss = Color.FromArgb(1, 0, 0);
+
+        public static Color mesa = Color.FromArgb(2, 0, 0);
+        public static Color mount = Color.FromArgb(3, 0, 0);
+        public static Color flatlandsh = Color.FromArgb(4, 0, 0);
+        public static Color flatlandsl = Color.FromArgb(5, 0, 0);
+
+
+        public static Bitmap TryGet(ICoreServerAPI sapi)
+        {
+            ICoreAPI coreApi = sapi;
+            if (map == null)
+            {
+                string mapFolder = coreApi.GetOrCreateDataPath("Maps");
+                string mapFile = mapFolder + @"/landforms.png";
+                if (File.Exists(mapFile))
+                {
+                    Bitmap bitmapRef = null;
+                    bitmapRef = new Bitmap(mapFile);
+                    return map = bitmapRef;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return map;
+            }
+        }
+
+
+        public static Bitmap TryGetClimate(ICoreServerAPI sapi)
+        {
+            ICoreAPI coreApi = sapi;
+            if (climateMap == null)
+            {
+                string mapFolder = coreApi.GetOrCreateDataPath("Maps");
+                string mapFile = mapFolder + @"/climate.png";
+                if (File.Exists(mapFile))
+                {
+                    Bitmap bitmapRef = null;
+                    bitmapRef = new Bitmap(mapFile);
+                    return climateMap = bitmapRef;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return climateMap;
+            }
+        }
+
+        // to be replaced with dictionary
+        public static int GetBiomeFromPixel(Color color)
+        {
+            if (color.R > 5)
+            {
+                return new Random().Next(6, 30);
+            }
+            else
+            {
+                return color.R;
+
+            }
+
+        }
+    }
+}
